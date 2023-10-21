d2b488e8ec7655eb6c02daec6bf8726b3dc02538
Muhammad Dedishov <moxmad99@mail.ru>
2023-10-22 01:42:01 +0300

    Added video and banner, also logic for opening banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './scss/app.scss'
+import ReactPlayer from 'react-player';
+import Banner from './components/Banner';
+
 
 function App() {
+
+  const [bannerOn, setBannerOn] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='screen'>
+      <ReactPlayer
+        onPlay={() => {
+          const timeout = setTimeout(() => {
+            setBannerOn(true);
+          }, 5000);
+
+          return () => clearTimeout(timeout);
+        }}
+        className='screen__video'
+        width='100%'
+        height='100%'
+        url='https://www.youtube.com/watch?v=M7FIvfx5J10'
+        playing
+      />
+      <Banner bannerState={bannerOn} />
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import qrCode from '../assets/img/qr-code.png'
+
+const Banner = ({ bannerState }: { bannerState: boolean }) => {
+    return (
+        <div className={bannerState ? "screen__banner screen__banner_open" : "screen__banner"}>
+            <p className="screen__banner-title">ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! <br />
+                ПОДАРИТЕ ЕМУ СОБАКУ!</p>
+            <img src={qrCode} alt="" />
+            <p className="screen__banner-text">Сканируйте QR-код <br />
+                или нажмите ОК</p>
+            <button className="screen__banner-button button">OK</button>
+        </div>
+    )
+}
+
+export default Banner
